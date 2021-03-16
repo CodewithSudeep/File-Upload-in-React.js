9a3d8588878f60ef2ee51dfb68558f3e1aed1331
sangya2001 <sangyasherpa2058@gmail.com>
2021-03-16 19:44:50 +0545

    Completed for mime type

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [file, setFile] = React.useState({ selectedFile: null, warning: false, uploaded: false })
+
+  // this function check if the type of file is image/png or jpeg or not
+  const handleselectedFile = e => {
+    switch (e.target.files[0].type) {
+      case 'image/png':
+        setFile({
+          selectedFile: e.target.files[0],
+          warning: false,
+          uploaded: true
+        })
+        break;
+
+      case 'image/jpeg':
+        setFile({
+          selectedFile: e.target.files[0],
+          warning: false,
+          uploaded: true
+        })
+        break;
+
+      // if not image return warning
+      default:
+        setFile({
+          selectedFile: null,
+          warning: true,
+          uploaded: false
+        })
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        file.warning && <div className="warning">Only Image format are allowed to upload!</div>
+      }
+      <input type="file" name="" id="" onChange={handleselectedFile} className="file-input" />
+      {
+        file.uploaded && <button>Upload</button>
+      }
     </div>
-  );
+  )
 }
-
-export default App;
